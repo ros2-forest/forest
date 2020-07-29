77799112a8424f9b4cb14761a6f0fbdd56c3e8de
ros2-forest <68951133+ros2-forest@users.noreply.github.com>
2020-07-29 14:17:53 -0300

    Update receive.py

--- examples/mnist_bnn/ros2_packages/mnist_bnn_send/mnist_bnn_send/receive.py
+++ examples/mnist_bnn/ros2_packages/mnist_bnn_send/mnist_bnn_send/receive.py
@@ -21,10 +21,10 @@ class RecNode(Node):
     def read_node_callback(self, msg):
         self.t4 = time.perf_counter()
         #self.get_logger().info("Label:{} Prediction:{}".format(msg.label, msg.digit[0]))
-        #self.get_logger().info("Prediction:{}".format(msg.digit[0]))
+        self.get_logger().info("Prediction:{}".format(msg.digit[0]))
         #if msg.label == msg.digit[0]:
         #    self.correct+=1
-        self.get_logger().info("{} - t4:{}".format(self.i, self.t4))
+        #self.get_logger().info("{} - t4:{}".format(self.i, self.t4))
         self.i+=1
         #self.get_logger().info("Accuracy: {}/{} ({:.2f}%)".format(self.correct, self.i, (self.correct/self.i)*100))
 
